1a97e525c1abccab9094432eac70f6dab0504afa
Nikola Markovi─ç <nikola.markovic@student.etf.unibl.org>
2024-03-16 19:55:18 +0100

    fix #2

--- P2/cmdarg1.c
+++ P2/cmdarg1.c
@@ -1,47 +1,64 @@
+// Program uzima ulazne argumente sa komandne linije i provlaci ih kroz dolenavedene funkcije tako sto se u
+// nizu nizova dinamickih stringova nalaze rezultantne vrijednosti provlacenja ulaznih argumenata kroz sve jednu
+// funkciju za transformaciju
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #define BUFF 1024
+#define NFUNC 3
 
-char *transformisi(char *rijec, char *(*f)(char *)) { return (*f)(rijec); };
+char **transformisi(char **, int, char *(*)(char *));
 char *invertuj(char *);
 char *skrati(char *);
 char *produzi(char *);
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-    char choice;
-    char **rez = (char **)malloc(argc * sizeof(char *));
-    for (int i = 1; i < argc; i++)
+    if (argc <= 1)
+        printf("Nema argumenata.");
+    else
     {
-        do
+        char choice;
+
+        char ***argvModarray = (char ***)calloc(NFUNC, sizeof(char **));
+        argvModarray[0] = transformisi(argv, argc, &skrati);
+        argvModarray[1] = transformisi(argv, argc, &produzi);
+        argvModarray[2] = transformisi(argv, argc, &invertuj);
+
+        for (int i = 0; i < NFUNC; i++)
+            free(argvModarray[i][0]); // zaostali prvi argumenti
+
+        for (int i = 1; i < argc; i++)
         {
-            printf("Sta uraditi sa parametrom '%s': ", argv[i]);
-            scanf("%c", &choice);
-        } while (choice != 'i' && choice != 's' && choice != 'p');
+            for (int j = 0; j < NFUNC; j++)
+            {
+                printf("argv[%d] == %s -> %s\n", i, argv[i], argvModarray[j][i]);
+                free(argvModarray[j][i]);
 
-        rez[i - 1] = (char *)malloc(BUFF * sizeof(char));
+                if (j == NFUNC - 1)
+                {
+                    free(argvModarray[j]);
+                    printf("====================================\n");
+                }
+            }
+        }
 
-        if (choice == 'i')
-            rez[i - 1] = transformisi(argv[i], &invertuj);
-        else if (choice == 's')
-            rez[i - 1] = transformisi(argv[i], &skrati);
-        else if (choice == 'p')
-            rez[i - 1] = transformisi(argv[i], &produzi);
-        choice = ' ';
+        free(argvModarray);
+        argvModarray = NULL;
     }
-
-    printf("\nTransformisani argumenti: ");
-    for (int i = 1; i < argc; i++)
-    {
-        printf("\nargv[%d] ==> %s -> %s", i, argv[i], rez[i - 1]);
-        free(rez[i - 1]);
-    }
-
-    free(rez);
-    rez = NULL;
 
     return 0;
+}
+
+char **transformisi(char **niz, int n, char *(*f)(char *))
+{
+    char **ret = (char **)calloc(n, sizeof(char *));
+
+    for (int i = 1; i < n; i++)
+        ret[i] = (*f)(niz[i]);
+
+    return ret;
 }
 
 char *invertuj(char *rijec)
@@ -62,17 +79,27 @@ char *skrati(char *rijec)
 
     do
     {
-        printf("Skrati za koliko: ");
+        printf("Skrati '%s' za koliko: ", rijec);
         scanf("%d", &s);
-    } while (s < 1 || s >= len);
+    } while (s < 0 || s >= len);
 
-    int newlen = len - s;
-    char *ret = (char *)calloc(newlen + 1, sizeof(char));
+    if (s)
+    {
+        int newlen = len - s;
+        char *ret = (char *)calloc(newlen + 1, sizeof(char));
 
-    for (int i = 0; i < newlen; i++)
-        ret[i] = rijec[i];
+        for (int i = 0; i < newlen; i++)
+            ret[i] = rijec[i];
 
-    return ret;
+        return ret;
+    }
+    else // ako se ne skracuje
+    {
+        char *ret = (char *)calloc(len + 1, sizeof(char));
+        strcpy(ret, rijec);
+
+        return ret;
+    }
 }
 
 char *produzi(char *rijec)
@@ -84,23 +111,33 @@ char *produzi(char *rijec)
 
     do
     {
-        printf("Produzi za koliko: ");
+        printf("Produzi '%s' za koliko: ", rijec);
         scanf("%d", &s);
-    } while (s < 1 || s >= maxlen - len);
+    } while (s < 0 || s >= maxlen - len);
 
-    do
+    if (s)
     {
-        printf("Unesi produzetak: ");
-        scanf("%s", prod);
-    } while (strlen(prod) < 1 || strlen(prod) > s);
+        do
+        {
+            printf("Unesi produzetak: ");
+            scanf("%s", prod);
+        } while (strlen(prod) < 1 || strlen(prod) > s);
 
-    int newlen = len + s;
-    char *ret = (char *)calloc(newlen + 1, sizeof(char));
-    strcpy(ret, rijec);
+        int newlen = len + s;
+        char *ret = (char *)calloc(newlen + 1, sizeof(char));
+        strcpy(ret, rijec);
 
-    for (int i = len, k = 0; i < newlen; i++, k++)
-        ret[i] = prod[k];
+        for (int i = len, k = 0; i < newlen; i++, k++)
+            ret[i] = prod[k];
 
-    free(prod);
-    return ret;
+        free(prod);
+        return ret;
+    }
+    else // ako se ne produzava
+    {
+        char *ret = (char *)calloc(len + 1, sizeof(char));
+        strcpy(ret, rijec);
+
+        return ret;
+    }
 }
