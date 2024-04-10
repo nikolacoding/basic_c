f4de80c97d15bbd37707644bd23ef37b490494e7
Nikola Markovi─ç <nikola.markovic@student.etf.unibl.org>
2024-04-10 22:59:19 +0200

    k1

---
+++ P2/K1/Zadaci sa kolokvijuma/2.c
@@ -0,0 +1,32 @@
+// U ulaznoj binarnoj datoteci upisani su podaci istog (nepoznatog) tipa. Napisati funkciju read sa promjenljivim
+// brojem argumenata koja prihvata naziv ulazne binarne datoteke (name), velicinu (s) jednog podatka u datoteci,
+// broj n te n pozicija (neobavezni argumenti). Funkcija read treba da ucita u dinamicku zonu memorije (tj. ucita
+// i formira dinamicki niz) n podataka iz ulazne binarne datoteke koji se nalaze na zadatim pozicijama. Funkcija
+// vraca adresu formiranog niza. Prototip funkcije je void *read(const char *name, int s, int n, ...);
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+void *read(const char *name, int s, int n, ...)
+{
+    FILE *fp;
+    void *ret;
+
+    if (fp = fopen(name, "rb"))
+    {
+        ret = (void *)malloc(n * s);
+        va_list args;
+        va_start(args, n);
+
+        for (int i = 0; i < n; i++)
+        {
+            int adr = va_arg(args, int);
+            fseek(fp, adr, SEEK_SET);
+            fread(&ret[i], s, 1, fp); // ovo ne kompajluje jbg
+        }
+    }
+
+    return ret;
+}
---
+++ P2/K1/Zadaci sa kolokvijuma/4.c
@@ -0,0 +1,59 @@
+// Neka je dat niz cjelobrojnih neoznacenih podataka koji su sortirani po vrijednosti. Implementirati funkciju:
+// unsigned int* ucitajBajt(unsigned int niz[], int duzina, int *n, unsigned int pocetak, unsigned int kraj);
+// U funkciji se nad nizom (parametar niz) vrsi binarna pretraga do segmenta vrijednosti koje pocinju ili su vece
+// od vrijednosti parametra kraj. Nakon pretrage vraca se dinamicki niz koji predstavlja segment originalnog
+// niza. U rezultujucem nizu su sve vrijednosti u opsegu izmedju parametara pocetak i kraj (ukljucujuci i njih),
+// pri cemu je duzina novog niza parametar *n.
+//
+// Smatrati da je pocetak <= kraj te da je vrijednost parametra kraj
+// jednaka ili manja od posljednje vrijednosti u nizu.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int *ucitajBajt(unsigned int niz[], int duzina, int *n, unsigned int pocetak, unsigned int kraj)
+{
+    int p = 0, k = duzina - 1, s;
+    unsigned int *ret = (unsigned int *)malloc(duzina * sizeof(unsigned int));
+
+    do
+    {
+        s = (p + k) / 2;
+
+        if (niz[s] >= pocetak)
+        {
+            while (niz[s - 1] >= pocetak && s - 1 >= 0)
+                s--;
+            break;
+        }
+
+    } while (p <= k);
+
+    ret[0] = niz[s];
+    int b = 1;
+
+    while (niz[s + 1] <= kraj && s - 1 < duzina)
+    {
+        ret[b++] = niz[s + 1];
+        s++;
+    }
+
+    *n = b;
+    ret = (unsigned int *)realloc(ret, b * sizeof(unsigned int *));
+
+    for (int i = 0; i < b; i++) // <- debug
+        printf("%d ", ret[i]);
+
+    return ret;
+}
+
+int main(int argc, char const *argv[])
+{
+    unsigned int niz[5] = {1, 2, 3, 4, 5};
+    int d;
+    unsigned int *retval;
+    ucitajBajt(niz, 5, &d, 1, 4);
+    printf("\nlen: %d\n", d);
+
+    return 0;
+}
